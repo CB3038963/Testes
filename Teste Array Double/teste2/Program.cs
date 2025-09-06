91dd7ed043afe24acff7766829153aeed9418945
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-09-06 17:23:58 -0300

    Add files via upload

---
+++ Teste Array Double/teste2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] valor = new double[10];
+            string resultado;
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write("Digite o valor do {0} Item: ", i);
+                resultado = Console.ReadLine();
+
+                if (!double.TryParse(resultado, out valor[i]))
+                {
+                    do
+                    {
+                        Console.WriteLine("Digite o valor do {0} Item novamente: ", i);
+                        resultado = Console.ReadLine();
+                    } while (!double.TryParse(resultado, out valor[i]));
+                }
+                
+
+
+            }
+        }
+    }
+}
