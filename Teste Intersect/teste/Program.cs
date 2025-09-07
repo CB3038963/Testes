26fc0ef772cef5854d8e5368d9d20c4cf7b82621
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-09-07 00:48:29 -0300

    Add files via upload

---
+++ Teste Intersect/teste/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] word1 = { "a", "b", "c" };
+            string[] word2 = { "p", "q", "r" };
+            
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("{0}{1}", word1[i], word2[i]);
+            }
+        }
+    }
+}
